a842939f87a0d773fe96e2fd5ff6a9d84f7729df
Sammy459 <gupta.samridh.484@gmail.com>
2023-05-01 21:52:33 +0530

    writing left shift

--- Assembler/ISA/B.py
+++ Assembler/ISA/B.py
@@ -41,6 +41,26 @@ def rs(instruction):
         except:
             return "ERROR: INVALID REGISTER CODE"
     
-        list[0].value=list[0].value<<int(list[1].value,2)
+        list[0].value=list[0].value<<int(list[1].value)
+        return f'{RS}_0_{list[0].__repr__()}_'+str(bin(list[1])[2:].zfill(7))
+
+def ls(instruction):
+    list = instruction.split()
+    if "FLAGS" in list:
+        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
+    if len(list) > 3:
+        return "ERROR: MORE THAN TWO OPERANDS GIVEN"
+    if len(list) < 3:
+        return "ERROR: INCOMPLETE INSTRUCTION"
+    
+    if list[0] != "rs":
+        return "ERROR: ILLEGAL ARGUMENT"
+    else:
+        try:
+            list=[eval("Const."+i) for i in list[1:]]
+        except:
+            return "ERROR: INVALID REGISTER CODE"
+    
+        list[0].value=list[0].value>>int(list[1].value)
         return f'{LS}_0_{list[0].__repr__()}_'+str(bin(list[1])[2:].zfill(7))
     
