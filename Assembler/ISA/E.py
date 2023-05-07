959b4d5b09dd600d13167d2543eee8680b5a2a4f
xegtor <rohakkansal04@gmail.com>
2023-05-07 20:35:10 +0530

    jump if less and jump if greater and jump if equal to complete

--- Assembler/ISA/E.py
+++ Assembler/ISA/E.py
@@ -42,3 +42,57 @@ def uncon_jmp(instruction):
         if list[1] not in branch:
             branch.append(list[1])
         return f'{UNCON_JUMP}_0000_{list[1].___repr__}'
+    
+def less_jmp(instruction):
+    list = instruction.split()
+    
+    if "FLAGS" in list:
+        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
+    if len(list) > 2:
+        return "ERROR: MORE THAN ONE OPERAND GIVEN"
+    if len(list) < 2:
+        return "ERROR: INCOMPLETE INSTRUCTION"
+    
+    if list[0] != "jlt":
+        return "ERROR: ILLEGAL ARGUMENT"
+    else:
+        list[1] = Branch(list[1])
+        if list[1] not in branch:
+            branch.append(list[1])
+        return f'{JUMP_LESS}_0000_{list[1].___repr__}'
+    
+def less_jmp(instruction):
+    list = instruction.split()
+    
+    if "FLAGS" in list:
+        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
+    if len(list) > 2:
+        return "ERROR: MORE THAN ONE OPERAND GIVEN"
+    if len(list) < 2:
+        return "ERROR: INCOMPLETE INSTRUCTION"
+    
+    if list[0] != "jgt":
+        return "ERROR: ILLEGAL ARGUMENT"
+    else:
+        list[1] = Branch(list[1])
+        if list[1] not in branch:
+            branch.append(list[1])
+        return f'{JUMP_GREAT}_0000_{list[1].___repr__}'
+    
+def less_jmp(instruction):
+    list = instruction.split()
+    
+    if "FLAGS" in list:
+        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
+    if len(list) > 2:
+        return "ERROR: MORE THAN ONE OPERAND GIVEN"
+    if len(list) < 2:
+        return "ERROR: INCOMPLETE INSTRUCTION"
+    
+    if list[0] != "je":
+        return "ERROR: ILLEGAL ARGUMENT"
+    else:
+        list[1] = Branch(list[1])
+        if list[1] not in branch:
+            branch.append(list[1])
+        return f'{JUMP_EQUAL}_0000_{list[1].___repr__}'
