95710bf1c0006c77d0e6b0c707ea861536818cbd
critz㌹ <pokemonmasterswapnil@gmail.com>
2023-05-07 19:11:33 +0530

    Bug fix

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -4,38 +4,7 @@ from ISA import B
 from ISA import C
 from ISA import D
 from ISA import E
-from ISA import F
-
-class Branch:
-    def __init__(self,label):
-        self.label=label
-        
-        if not Const.Mem:
-            self.address=Const.Mem[-1]+0b1
-        else:
-            self.address=0b000_0000
-    
-    def __repr__(self):
-        return self.address
-    
-    def __str__(self):
-        return self.label
-    
-    def function(self,instruction):
-        for i in instruction:
-            if i.split()[0]=='add':
-                A.add(i)
-            elif i.split()[0]=='sub':
-                A.sub(i)
-            elif i.split()[0]=='mul':
-                A.mul(i)
-            elif i.split()[0]=='xor':
-                A.xor(i)
-            elif i.split()[0]=='or':
-                A._or_(i)
-            elif i.split()[0]=='and':
-                A._and_(i)
-            
+from ISA import F        
 
 for i in Const.Reg:
     print(i.__repr__())
@@ -48,7 +17,7 @@ print(B.right_shift("rs R0 $10"))
 print(Const.R0)
 print(D.var("var xyz"))
 
-B1=Branch("label1")
+B1=E.Branch("label1")
 B1.function(["add R0 R1 R2"])
 
 print(Const.R0)
--- Assembler/ISA/E.py
+++ Assembler/ISA/E.py
@@ -5,3 +5,21 @@ JUMP_LESS = Const.Opcode(0b11100)
 JUMP_GREAT = Const.Opcode(0b11101)
 JUMP_EQUAL = Const.Opcode(0b11111)
 
+class Branch:
+    def __init__(self,label):
+        self.label=label
+        
+        if not Const.Mem:
+            self.address=Const.Mem[-1]+0b1
+        else:
+            self.address=0b000_0000
+    
+    def __repr__(self):
+        return self.address
+    
+    def __str__(self):
+        return self.label
+    
+    def function(self,instruction):
+        return self,instruction
+
