73e53459bb902c791bd842f2e59b65afd430b09a
xegtor <rohakkansal04@gmail.com>
2023-05-07 20:25:04 +0530

    unconditional jump function complete

--- Assembler/ISA/E.py
+++ Assembler/ISA/E.py
@@ -5,6 +5,8 @@ JUMP_LESS = Const.Opcode(0b11100)
 JUMP_GREAT = Const.Opcode(0b11101)
 JUMP_EQUAL = Const.Opcode(0b11111)
 
+branch = []
+
 class Branch:
     def __init__(self,label):
         self.label=label
@@ -22,4 +24,21 @@ class Branch:
     
     def function(self,instruction):
         return self,instruction
-
+    
+def uncon_jmp(instruction):
+    list = instruction.split()
+    
+    if "FLAGS" in list:
+        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
+    if len(list) > 2:
+        return "ERROR: MORE THAN ONE OPERAND GIVEN"
+    if len(list) < 2:
+        return "ERROR: INCOMPLETE INSTRUCTION"
+    
+    if list[0] != "jmp":
+        return "ERROR: ILLEGAL ARGUMENT"
+    else:
+        list[1] = Branch(list[1])
+        if list[1] not in branch:
+            branch.append(list[1])
+        return f'{UNCON_JUMP}_0000_{list[1].___repr__}'
