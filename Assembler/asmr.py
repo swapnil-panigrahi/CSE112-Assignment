af3119ca6f77fb3497060d157346479f5a4518c9
critz㌹ <raghav22385@iiitd.ac.in>
2023-05-10 20:17:41 +0530

    Assembler done

--- Assembler/asmr.py
+++ Assembler/asmr.py
@@ -1,5 +1,4 @@
 import os
-import time
 from ISA import Constants as Const
 from ISA import A
 from ISA import B
@@ -9,87 +8,91 @@ from ISA import E
 from ISA import F        
 
 def instruction_decode(instruction):
-        instr=instruction.strip().split()[0].strip(':')
+        instr_header=instruction.strip().split()[0].strip(':')
 
-        if instr=="add":
+        if instr_header=="add":
             return A.add(instruction.strip())
-        elif instr=='sub':
+        elif instr_header=='sub':
             return A.sub(instruction.strip())
-        elif instr=='mul':
+        elif instr_header=='mul':
             return A.mul(instruction.strip())
-        elif instr=='xor':
+        elif instr_header=='xor':
             return A.xor(instruction.strip())
-        elif instr=='or':
+        elif instr_header=='or':
             return A._or_(instruction.strip())
-        elif instr=='and':
+        elif instr_header=='and':
             return A._and_(instruction.strip())
-        elif instr=='mov':
+        elif instr_header=='mov':
             if "$" in instruction:
                 return B.movi(instruction.strip())
             else:
                 return C.movr(instruction.strip())
-        elif instr=='rs':
+        elif instr_header=='rs':
             return B.right_shift(instruction.strip())
-        elif instr=='ls':
+        elif instr_header=='ls':
             return B.left_shift(instruction.strip())
-        elif instr=='div':
+        elif instr_header=='div':
             return C.div(instruction.strip())
-        elif instr=='not':
+        elif instr_header=='not':
             return C.inv(instruction.strip())
-        elif instr=='cmp':
+        elif instr_header=='cmp':
             return C.comp(instruction.strip())
-        elif instr=='var':
+        elif instr_header=='var':
             return D.var(instruction.strip())
-        elif instr=='ld':
+        elif instr_header=='ld':
             return D.load(instruction.strip())
-        elif instr=='st':
+        elif instr_header=='st':
             return D.store(instruction.strip())
-        elif instr=='jmp':
+        elif instr_header=='jmp':
             return E.uncon_jmp(instruction.strip())
-        elif instr=='jlt':
+        elif instr_header=='jlt':
             return E.less_jmp(instruction.strip())
-        elif instr=='jgt':
+        elif instr_header=='jgt':
             return E.greater_jmp(instruction.strip())
-        elif instr=='je':
+        elif instr_header=='je':
             return E.equal_jmp(instruction.strip())
-        elif instr=='hlt':
+        elif instr_header=='hlt':
             return F.hlt(instruction.strip())
         else:
-            if instr not in E.br_var:
-                return f'INVALID INSTRUCTION OR BRANCH PASSED'
+            if instr_header not in E.br_var:
+                return 'INVALID INSTRUCTION OR BRANCH PASSED'
             else:
                 for j in instruction.split(': ')[1:]:
                     return instruction_decode(j.strip())
 
-test_file = open(r'Assembler/tests/errorGen/test1', "r")
-instr_list=test_file.readlines()
-# for i in instr_list:
-#     print(instruction_decode(i.strip()))
+cwd=os.getcwd()
+test_files=os.listdir(f'{cwd}/CSE112-Assignment/Assembler/tests/input_cases')
 
-errorgen_files=os.listdir("D:\CSE112-Assignment\Assembler\\tests\errorGen")
-hardbin_files=os.listdir("D:\CSE112-Assignment\Assembler\\tests\hardBin")
-simplebin_files=os.listdir("D:\CSE112-Assignment\Assembler\\tests\simpleBin")
+for i in test_files:
+    files=os.listdir(f'{cwd}/CSE112-Assignment/Assembler/tests/input_cases/{i}')
+    
+    try:
+        os.mkdir(f'{cwd}/CSE112-Assignment/Assembler/tests/output_cases/{i}')
+    except:
+        pass
+    
+    for j in files:
+        with open(f'{cwd}/CSE112-Assignment/Assembler/tests/input_cases/{i}/{j}') as test_case:
+            instr_list=test_case.readlines()
+            if 'hlt' in instr_list[0]:
+                break
+            
+            bin_return=[]
+            
+            for k in instr_list:
+                if k.strip():
+                    bin_str=instruction_decode(k)
 
-for i in errorgen_files:
-    f=open(f"D:\CSE112-Assignment\Assembler\\tests\errorGen\{i}", "r")
-    for j in f:
-        if j=="\n":
-            pass
-        else:
-            print(instruction_decode(j))
-
-for i in hardbin_files:
-    f=open(f"D:\CSE112-Assignment\Assembler\\tests\hardBin\{i}", "r")
-    for j in f:
-        if j=="\n":
-            pass
-        else:
-            print(instruction_decode(j))
-
-for i in simplebin_files:
-    f=open(f"D:\CSE112-Assignment\Assembler\\tests\simpleBin\{i}", "r")
-    for j in f:
-        if j=="\n":
-            pass
-        else:
-            print(j, instruction_decode(j))
+                    if bin_str!=None and 'ERROR' not in bin_str:
+                        bin_return.append(bin_str+'\n')
+                    elif bin_str==None:
+                        pass
+                    else:
+                        print(k.strip(),bin_str)
+                        break            
+            
+            else:
+                output_case=open(f'{cwd}/CSE112-Assignment/Assembler/tests/output_cases/{i}/{j}.txt',"w")
+                
+                output_case.writelines(bin_return)
+                output_case.close()
