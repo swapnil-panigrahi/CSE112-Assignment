96e11204a41f3809a9a7a8b1e4de1c89a62be1da
Sammy459 <gupta.samridh.484@gmail.com>
2023-06-04 13:51:12 +0530

    changes in d type instruction

--- SimpleSimulator/ISA/D.py
+++ SimpleSimulator/ISA/D.py
@@ -21,51 +21,20 @@ def var(instruction):
         Const.Mem[list[1].var]=list[1].address
         
         Const.Mem_block.append(list[1].address)
-            
-        #return f'{list[1].__repr__()}' 
+
 
 def load(instruction):
-    list = instruction.split()
+    list = [instruction[6:9]]+[instruction[9:]]
     
-    if "FLAGS" in list:
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN ONE OPERAND GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
+    list[0]=Const.decode_register(list[0])
+    list[1]=Const.decode_memory(list[1])
     
-    if list[0] != "ld":
-        return "ERROR: ILLEGAL ARGUMENT"
-    else:
-        try:
-            t = eval("Const."+list[1])
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-        for i in Const.Mem:
-            if i == list[2] and i not in E.br_var:
-                t.value = Const.Mem[i]
-                return f'{LOAD}0{t.__repr__()}{bin(abs(Const.Mem[i]))[2:].zfill(7)}'
-        return "ERROR: USE OF NOT DECLARED VARIABLE"
+    list[0].value=list[1]
     
 def store(instruction):
-    list = instruction.split()
-
-    if "FLAGS" in list:
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN ONE OPERAND GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
+    list = [instruction[6:9]]+[instruction[9:]]
+        
+    list[0]=Const.decode_register(list[0])
+    list[1]=Const.decode_memory(list[1])
     
-    if list[0] != "st":
-        return "ERROR: ILLEGAL ARGUMENT"
-    else:
-        try:
-            t = eval("Const."+list[1])
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-        for i in Const.Mem:
-            if i == list[2] and i not in E.br_var:
-                #Const.Mem[i] = t.value
-                return f'{STORE}0{t.__repr__()}{bin(abs(Const.Mem[i]))[2:].zfill(7)}'
-        return "ERROR: USE OF NOT DECLARED VARIABLE"
+    Const.Mem[list[1]]=list[0].value
