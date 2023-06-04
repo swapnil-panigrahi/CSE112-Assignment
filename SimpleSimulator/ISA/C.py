5952e1cb3737af385a5d30ecee4cfc4e9260e4f8
Sammy459 <gupta.samridh.484@gmail.com>
2023-06-04 17:02:24 +0530

    changes in c type instructions

--- SimpleSimulator/ISA/C.py
+++ SimpleSimulator/ISA/C.py
@@ -26,46 +26,22 @@ def div(instruction):
         Const.R1=list[0]%list[1]
     
 def inv(instruction):
-    list = instruction.split()
-    if "FLAGS" in list:
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN THREE OPERANDS GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
+    list = [instruction[10:13]]+[instruction[13:]]
+    for i in list:
+        i=Const.decode_register(i)
+        
+    list[0] = -(~(list[1].value))
     
-    if list[0] != "not":
-        return "ERROR: ILLEGAL ARGUMENT"
-    else:
-        try:
-            list=[eval("Const."+i) for i in list[1:]]
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-    
-        list[0].value=~(list[1].value)
-        return f'{NOT}00000{list[0].__repr__()}{list[1].__repr__()}'
-
 def comp(instruction):
-    list = instruction.split()
-    if "FLAGS" in list:
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN THREE OPERANDS GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
-    
-    if list[0] != "cmp":
-        return "ERROR: ILLEGAL ARGUMENT"
+    list = [instruction[10:13]]+[instruction[13:]]
+    for i in list:
+        i=Const.decode_register(i)
+        
+    if list[0].value > list[1].value:
+        Const.FLAGS.greater_than()
+        
+    elif list[0].value < list[1].value:
+        Const.FLAGS.less_than()
+        
     else:
-        try:
-            list=[eval("Const."+i) for i in list[1:]]
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-    
-        if(list[0].value>list[1].value):
-            Const.FLAGS.greater_than()
-        elif(list[0].value<list[1].value):
-            Const.FLAGS.less_than()
-        else:
-            Const.FLAGS.equal()
-        return f'{COMP}00000{list[0].__repr__()}{list[1].__repr__()}'
+        Const.FLAGS.equal()
