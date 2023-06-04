ea197235be6d3fff774eb9f595b691515f55dd8f
Sammy459 <gupta.samridh.484@gmail.com>
2023-06-04 16:19:47 +0530

    c type instructions

--- SimpleSimulator/ISA/C.py
+++ SimpleSimulator/ISA/C.py
@@ -6,51 +6,24 @@ NOT=Const.Opcode(0b01101)
 COMP=Const.Opcode(0b01110)
 
 def movr(instruction):
-    list = instruction.split()
-    if "FLAGS" in list and not list[2]=="FLAGS":
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN THREE OPERANDS GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
+    list = [instruction[10:13]]+[instruction[13:]]
+    for i in list:
+        i=Const.decode_register(i)
     
-    if list[0] != "mov":
-        return "ERROR: ILLEGAL ARGUMENT"
-    else:
-        try:
-            list=[eval("Const."+i) for i in list[1:]]
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-    
-        list[0].value=list[1].value
-        return f'{MOVR}00000{list[0].__repr__()}{list[1].__repr__()}'
+    list[0].value=list[1].value
         
 def div(instruction):
-    list = instruction.split()
-    if "FLAGS" in list:
-        return "ERROR: FLAGS CANNOT BE AN OPERAND HERE"
-    if len(list) > 3:
-        return "ERROR: MORE THAN THREE OPERANDS GIVEN"
-    if len(list) < 3:
-        return "ERROR: INCOMPLETE INSTRUCTION"
-    
-    if list[0] != "div":
-        return "ERROR: ILLEGAL ARGUMENT"
+    list = [instruction[10:13]]+[instruction[13:]]
+    for i in list:
+        i=Const.decode_register(i)
+        
+    if list[1].value==0:
+        Const.FLAGS.overflow()
+        Const.R0=0
+        Const.R1=0
     else:
-        try:
-            list=[eval("Const."+i) for i in list[1:]]
-        except:
-            return "ERROR: INVALID REGISTER CODE"
-
-        if list[1].value == 0b0000_0000_0000_0000:
-            Const.R0.value=0b0000_0000_0000_0000
-            Const.R1.value=0b0000_0000_0000_0000
-            Const.FLAGS.overflow()
-        else:
-            Const.R0.value=list[0].value//list[1].value
-            Const.R1.value=list[0].value%list[1].value
-            
-        return f'{DIV}00000{list[0].__repr__()}{list[1].__repr__()}'
+        Const.R0=list[0]//list[1]
+        Const.R1=list[0]%list[1]
     
 def inv(instruction):
     list = instruction.split()
